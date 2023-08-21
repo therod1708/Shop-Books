88c05028814a1f5704f2971137e0071e05208ff3
Rodrigo Viana <130801290+therod1708@users.noreply.github.com>
2023-08-21 11:40:32 -0300

    Adicionando arquivos ao Shop-Books

---
+++ main.js
@@ -0,0 +1,7 @@
+// const placeholder = document.querySelectorAll("[data-placeholder]");
+
+// placeholder.forEach( (elemento) => {
+//     elemento.addEventListener('click', (i) => {
+//         console.log(i.target);
+//     })
+// });
