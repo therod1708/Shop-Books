e31ff5cde17c1793a0292297bd3eda55d2271b35
Rodrigo Viana <rod1992br@gmail.com>
2023-08-21 12:06:23 -0300

    Deletando o main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// const placeholder = document.querySelectorAll("[data-placeholder]");
-
-// placeholder.forEach( (elemento) => {
-//     elemento.addEventListener('click', (i) => {
-//         console.log(i.target);
-//     })
-// });
